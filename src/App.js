46822ed2fda5cec6781cb93e509d54464d0ca84e
gayatri <gayatrisanjayshelke20@navgurukul.org>
2022-08-20 22:33:30 +0530

    initialse project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from'react'
+import Delete from '@material-ui/icons/Delete'
+import {Button} from '@material-ui/core'
 function App() {
+  const [color,setColor]=useState('primary')
+  const [disablebtn,setDisablebtn]=useState(false)
+  function onclicked(){
+    // setColor('secondary')
+    setDisablebtn(true)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello</h1>
+    <Button color={color} disabled={disablebtn} startIcon={<Delete/>} variant="contained" 
+    onClick={()=>{onclicked()}}>Default</Button>
+
     </div>
   );
 }
 
 export default App;
+
